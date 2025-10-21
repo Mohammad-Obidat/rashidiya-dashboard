159b712934749df6d3389eaeccf9b685042e135c
Mohammad <mo.s.obidat@gmail.com>
2025-10-21 16:39:22 +0300

    enableCORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,23 +5,21 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
-  );
-
-  // Enable CORS for your frontend
   app.enableCors({
-    origin: 'https://rashidiya-dashboard.vercel.app', // || 'http://localhost:5173',
+    origin: ['https://rashidiya-dashboard.vercel.app', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
-  await app.listen(process.env.PORT || 3000);
-  console.log(
-    `Application is running on: http://localhost:${process.env.PORT || 3000}`,
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+
+  await app.listen(process.env.PORT || 3000);
+  console.log(`Application is running on port: ${process.env.PORT || 3000}`);
 }
+
 bootstrap().catch((err) => {
   console.error('Error during bootstrap:', err);
   process.exit(1);
