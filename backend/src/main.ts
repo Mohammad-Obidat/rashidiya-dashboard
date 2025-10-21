fc5122c1bf0c33944df01d87a4aef95056df4b0f
Mohammad <mo.s.obidat@gmail.com>
2025-10-21 16:13:01 +0300

    allowedHeaders

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     origin: 'https://rashidiya-dashboard.vercel.app', // || 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    // allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   await app.listen(process.env.PORT || 3000);
