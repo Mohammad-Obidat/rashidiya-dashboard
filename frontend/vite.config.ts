efd8d7b62e7bf5fe2428e815c7094b8ea68a4560
Mohammad <mo.s.obidat@gmail.com>
2025-10-22 20:33:03 +0300

    Applying PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,44 @@
 import { defineConfig } from 'vite';
+// import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   injectRegister: 'auto',
+    //   workbox: {
+    //     globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}'],
+    //   },
+    //   manifest: {
+    //     name: 'Rashidiya Dashboard',
+    //     short_name: 'Dashboard',
+    //     description: 'A dashboard for managing Rashidiya programs.',
+    //     theme_color: '#ffffff',
+    //     icons: [
+    //       {
+    //         src: 'pwa-192x192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png',
+    //       },
+    //       {
+    //         src: 'pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //       },
+    //       {
+    //         src: 'pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //         purpose: 'any maskable',
+    //       },
+    //     ],
+    //   },
+    // }),
+  ],
   server: {
     port: 5173,
     proxy: {
